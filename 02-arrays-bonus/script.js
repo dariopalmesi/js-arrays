29ce4b6fb753295baf541f907660a34481915385
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-02 16:51:21 +0200

    array bonus commit

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,10 +10,17 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.reverse();
+console.log(teachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
 
 // 3. Rimuovi 'Ed' dall'array teachers
+let index = teachers.indexOf('Ed')
+if (index !== -1) {
+  teachers.splice(index,1)
+}
+console.log(index);
+console.log(teachers);
+
