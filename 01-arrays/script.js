caa56a804774e6ea2ee01c35d019ea80082dc7e1
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-02 15:42:52 +0200

    upgrade array commit

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,9 +10,8 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers.indexOf('Carlo')
-console.log(fourthTeacher);
-
+const fourthTeacher = ['Nathan','Ed','Fabio','Phil','Carlo','Lewis','Luca']
+console.log(fourthTeacher[4]);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
